72ef18c818e9c271f0cdae49200620f5044fd2d9
Johannes Nam <nam.johannes@gmail.com>
2019-06-15 17:39:58 +0200

    Nest circles and texts into svgs.

--- circle.js
+++ circle.js
@@ -1,30 +1,30 @@
-const n = 16;
+const n = 11;
 const balls = new Array(n).fill(1).map( (d, i) => { return i+1; } );
-var current_balls = balls;
 const k = 2*Math.PI/n;
 const R = 200, r = R/10;
 const w = 600, h = 600;
+const blue = "#5592fc";
+const red = "#fc5555";
+const green = "#97ef83";
 
 var index = 0;
 
 const init = function() {
   var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
   
-  var circles = svg.append("g").attr("id", "groupOfBalls")
-    .selectAll("cirle")
+  var players = svg.append("g").attr("id", "groupOfPlayers")
+    .selectAll("svg")
     .data(balls)
-    .enter().append("circle")
-    .attr("cx", (d) => { return w/2 - R*Math.cos((d-1)*k); })
-    .attr("cy", (d) => { return h/2 - R*Math.sin((d-1)*k); })
-    .attr("r",  r)
-    .attr("fill", "blue");
+    .enter().append("svg").classed("player", true)
+      .attr("overflow", "visible")
+      .attr("x", (d) => { return w/2 - R*Math.cos((d-1)*k); })
+      .attr("y", (d) => { return h/2 - R*Math.sin((d-1)*k); })
 
-  var labels = svg.append("g").attr("id", "groupOfLabels")
-    .selectAll("text")
-    .data(balls)
-    .enter().append("text")
-    .attr("x", (d) => { return w/2 - R*Math.cos((d-1)*k); })
-    .attr("y", (d) => { return h/2 - R*Math.sin((d-1)*k); })
+  var circles = players.append("circle")
+    .attr("r",  r)
+    .attr("fill", blue);
+
+  var labels = players.append("text")
     .text( (d) => { return d; } )
     .attr("text-anchor", "middle")
     .attr("alignment-baseline", "middle")
@@ -32,39 +32,42 @@ const init = function() {
     .attr("font-family", "sans-serif")
     .attr("font-size", "10px");
 
-  d3.select("circle:nth-child(" + balls[index] + ")").attr("fill", "green");
+  d3.select("circle:nth-child(" + balls[index] + ")").attr("fill", green);
 }
 
 const move = function() {
-  let l = current_balls.length;
+  let l = balls.length;
   if ( l === 1 ) {
     return false;
   } else {
-    let active_ball = current_balls[index];
+    let active_ball = d3.select(".player:nth-child(" + balls[index] + ") circle")
     index = (index+1) % l;
-    let dismissed_ball = current_balls.splice(index, 1)[0];
+    let dismissed_ball = d3.select(".player:nth-child(" + balls.splice(index, 1)[0] + ")")
     l--;
     index = index % l;
-    let next_active_ball = current_balls[index];
+    let next_active_ball = d3.select(".player:nth-child(" + balls[index] + ") circle")
 
-    d3.select("circle:nth-child(" + active_ball + ")").attr("fill", "blue");
-    d3.select("circle:nth-child(" + dismissed_ball + ")")
-      .transition()
-        .attr("delay", () => { return 200; })
-        .attr("duration", () => { return 9000; })
-        .attr("fill", "red")
-        .attr("r", 0.5*r)
-        .attr("cx", (d) => { return w/2 - (R+3*r)*Math.cos((d-1)*k); })
-        .attr("cy", (d) => { return h/2 - (R+3*r)*Math.sin((d-1)*k); });
+    active_ball.transition()
+      .attr("delay", 200)
+      .attr("duration", 1000)
+      .attr("fill", blue);
 
-    d3.select("circle:nth-child(" + next_active_ball + ")").attr("fill", "green");
+    dismissed_ball.transition()
+      .attr("delay", 200)
+      .attr("duration", 1000)
+      .attr("x", (d) => { return w/2 - (R+3*r)*Math.cos((d-1)*k); })
+      .attr("y", (d) => { return h/2 - (R+3*r)*Math.sin((d-1)*k); });
 
-    d3.select("text:nth-child(" + dismissed_ball + ")")
-      .transition()
-        .attr("delay", () => { return 200; })
-        .attr("duration", () => { return 9000; })
-        .attr("x", (d) => { return w/2 - (R+3*r)*Math.cos((d-1)*k); })
-        .attr("y", (d) => { return h/2 - (R+3*r)*Math.sin((d-1)*k); });
+    dismissed_ball.select("circle").transition()
+      .attr("delay", 200)
+      .attr("duration", 1000)
+      .attr("fill", red)
+      .attr("r", 0.5*r);
+
+    next_active_ball.transition()
+      .attr("delay", 200)
+      .attr("duration", 1000)
+      .attr("fill", green);
 
     return true;
   }
@@ -77,7 +80,7 @@ const play = function() {
     } else {
       console.log("winner");
     } 
-  }, 1000 );
+  }, 1500 );
 }
 
 init();
