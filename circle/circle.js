cf83c3c3a644702365fa943f4744c0d95d754252
Johannes Nam <nam.johannes@gmail.com>
2019-06-17 10:56:46 +0200

    Remove players when hit.

--- circle/circle.js
+++ circle/circle.js
@@ -1,38 +1,42 @@
-const n = 11;
+const n = 20;
 const balls = new Array(n).fill(1).map( (d, i) => { return i+1; } );
 const k = 2*Math.PI/n;
-const R = 200, r = R/10;
+const R = 200, r = R/15;
 const w = 600, h = 600;
 const blue = "#5592fc";
 const red = "#fc5555";
-const green = "#97ef83";
 
 var index = 0;
 
 const init = function() {
   var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
   
-  var players = svg.append("g").attr("id", "groupOfPlayers")
+  var players = svg
+    .append("g")
+      .attr("id", "groupOfPlayers")
     .selectAll("svg")
-    .data(balls)
-    .enter().append("svg").classed("player", true)
+      .data(balls)
+      .enter()
+    .append("svg")
+      .classed("player", true)
       .attr("overflow", "visible")
       .attr("x", (d) => { return w/2 - R*Math.cos((d-1)*k); })
-      .attr("y", (d) => { return h/2 - R*Math.sin((d-1)*k); })
+      .attr("y", (d) => { return h/2 - R*Math.sin((d-1)*k); });
 
   var circles = players.append("circle")
     .attr("r",  r)
-    .attr("fill", blue);
+    .attr("fill", blue)
+    .attr("stroke", "black");
 
   var labels = players.append("text")
     .text( (d) => { return d; } )
     .attr("text-anchor", "middle")
     .attr("alignment-baseline", "middle")
     .attr("fill", "white")
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "10px");
+    .attr("font-family", "'Swanky and Moo Moo', cursive")
+    .attr("font-size", "20px");
 
-  d3.select("circle:nth-child(" + balls[index] + ")").attr("fill", green);
+  d3.select("circle:nth-child(" + balls[index] + ")").attr("fill", red);
 }
 
 const move = function() {
@@ -55,19 +59,14 @@ const move = function() {
     dismissed_ball.transition()
       .attr("delay", 200)
       .attr("duration", 1000)
-      .attr("x", (d) => { return w/2 - (R+3*r)*Math.cos((d-1)*k); })
-      .attr("y", (d) => { return h/2 - (R+3*r)*Math.sin((d-1)*k); });
-
-    dismissed_ball.select("circle").transition()
-      .attr("delay", 200)
-      .attr("duration", 1000)
-      .attr("fill", red)
-      .attr("r", 0.5*r);
+      .attr("x", (d) => { return w/2 - (R+5*r)*Math.cos((d-1)*k); })
+      .attr("y", (d) => { return h/2 - (R+5*r)*Math.sin((d-1)*k); })
+      .attr("opacity", 0);
 
     next_active_ball.transition()
       .attr("delay", 200)
       .attr("duration", 1000)
-      .attr("fill", green);
+      .attr("fill", red);
 
     return true;
   }
